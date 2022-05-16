7f3b731fcb162d2c79bf3d1947b6108bc3871e3c
Matej Petras <matej.petras@tholyn.sk>
2022-05-16 16:21:34 +0200

    Added CORS configuration. Cleaned up schema.graphqls. WIP

--- src/main/java/sk/catheaven/graphqlserver/resolvers/BooksResolver.java
+++ src/main/java/sk/catheaven/graphqlserver/resolvers/BooksResolver.java
@@ -10,12 +10,11 @@ import java.util.List;
 public class BooksResolver implements GraphQLQueryResolver {
     public List<Book> allBooks() {
         return List.of(
-                new Book(1L, "A", 10, 1L),
-                new Book(2L, "B", 100, 2L),
-                new Book(3L, "C", 11, 1L),
-                new Book(4L, "D", 111, 1L),
-                new Book(5L, "E", 1234, 2L)
-
+            new Book(1L, "A", 10, 1L),
+            new Book(2L, "B", 100, 2L),
+            new Book(3L, "C", 11, 1L),
+            new Book(4L, "D", 111, 1L),
+            new Book(5L, "E", 1234, 2L)
         );
     }
 }
