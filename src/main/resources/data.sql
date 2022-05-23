cc7e143149ba064ae8a5be3ac061775f04fa4957
Matej Petras <matej.petras@tholyn.sk>
2022-05-23 15:35:07 +0200

    Added persistence and JPA dependencies and configured data generation.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+INSERT INTO fish (name)
+            VALUES ('Kapor'),
+                   ('Štuka'),
+                   ('Sumec'),
+                   ('Zubáč'),
+                   ('Ostriež'),
+                   ('Pleskáč vysoký'),
+                   ('Lieň sliznatý'),
+                   ('Jalec hlavatý'),
+                   ('Boleň'),
+                   ('Amur'),
+                   ('Karas'),
+                   ('Úhor'),
+                   ('Mieň'),
+                   ('Tolstolobik'),
+                   ('Mrena severná'),
+                   ('Nosáľ'),
+                   ('Iné druhy');
+
+INSERT INTO fishing_ground(code, label)
+       VALUES ('0010', 'Bobrie jazero'),
+              ('0910', 'Štrkovisko Gajarské'),
+              ('0911', 'Štrkovisko Suchohrad 1'),
+              ('0912', 'Štrkovisko Suchohrad 2'),
+              ('1470', 'Záhorský kanál'),
+              ('0860', 'Špek'),
+              ('0861', 'Kruh'),
+              ('0862', 'Junkov Dolec'),
+              ('0863', 'Habán'),
+              ('0864', 'Strža'),
+              ('0866', 'Štelc a Výkopa'),
+              ('0480', 'Požiarna nádrž'),
+              ('0410', 'Morava č. 3');
